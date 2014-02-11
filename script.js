4350166d7de7051224c11e0af01bb22b9ab4c818
ashishghai <me@ashishghai.com>
2014-02-11 21:14:18 +0530

    made changes to script.js
    made changes to script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var album = function (name,year,artist,genre)
+{
+
+this.name=name;
+this.year=year;
+this.artist=artist;
+this.genre=genre;
+
+}
+
+var track = function(track_title,track,artist,year,time_length)
+{
+	this.track_title =title;
+	this.track=track;
+	this.artist=artist;
+	this.year=year;
+	this.time_length=length;
+
+}
